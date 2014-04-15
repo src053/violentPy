97493bc3280dfc3b087a47029ab39f0ddd8be05a
src053 <src053@gmail.com>
2014-04-15 06:30:29 -0700

    first commit from mint

---
+++ chap1/chaos.py
@@ -0,0 +1,11 @@
+#A simple program illustration chaotic behavior.
+#Example from Python programing
+
+def main(): # define the function main
+	print ("this program illustrates a caotic function") #print somethign to the user
+	x = eval(input("Enter a number between 0 and 1: ")) #print question to user then assign the input to x var
+	for i in range(10): # loop 10 times with the for loop
+		x = 3.9 * x * (1 - x) #re-define the x var with new evaluation
+		print (x) #print the value of x to user
+
+main() #call main function
