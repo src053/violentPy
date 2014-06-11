d2fdbf8ac6c4272c5be63be4dbece9daf41be3a1
src053@gmail.com <src053@gmail.com>
2014-06-11 06:33:28 -0700

    2014-06-11:06:33

--- chap8/windChill.py
+++ chap8/windChill.py
@@ -11,12 +11,28 @@ def main():
 
 
 	#print the top row
-	print("-(-20)-(-10)-(0)-(10)-(20)-(30)-(40)-(50)-(60)")
+	print("-(-20)-(-10)-( 0)-(10)-(20)-(30)-(40)-(50)-(60)")
 	print("_______________________________________________")
 
-	while wind < 50:
+	while wind <= 50:
 		#var for temp
 		temp = -20
+		row = []
 		#check that wind speed is above 3mph
-		if wind > 3:
-			
+		if wind < 3:
+			i = 0
+			while i <= 9:
+				row.append("NA  ")
+				i = i + 1
+			print(wind, ''.join(map(str, row)))
+
+		else:
+			while temp <= -60:
+				row.append(round(windChill(wind, temp)))
+				print(wind, '   '.join(map(str, row)))
+				temp = temp + 10
+		wind = wind + 5
+
+
+main()
+				
