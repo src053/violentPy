4dd6d9003e3f80515f9491f6af4fa7b4a72869d1
src053 <src053@gmail.com>
2014-07-05 15:35:51 -0700

    2014-07-05:15:35

--- chap2/bannerGrab.py
+++ chap2/bannerGrab.py
@@ -24,23 +24,52 @@ def connScann(tgtHost, tgtPort):
 		print '[-]%d/tcp closed'% tgtPort
 
 #function to scan the IP provided to the script
-def portScan(tgtHost, tgtPort):
+def portScan(tgtHost, tgtPorts):
 	#try the socket function to get host by name (from hostname)
 	try:
 		#this will resolve hostname to IP address
 		tgtIP = gethostbyname(tgtHost)
 	except:
 		#print that tgt host is not reachable
-		print '[-] Cannot resolve '%s': Unknown host'%tgtHost
+		print '[-] Cannot resolve "%s": Unknown host'%tgtHost
 		return
 	try:
-		#this will resolve IP to host name
+		#var after resolving IP to host name
 		tgtName = gethostbyaddr(tgtIP)
+		#print the result of target name
+		print'\n[+] Scan Results for: ' + tgtName[0]
+	except:
+		#print the IP address
+		print'\n[+] Scan Results for: ' + tgtIP
+	#set the socket default timeout
+	setdefaulttimeout(1)
+	#iterate through all tgt port
+	for tgtPort in tgtPorts:
+		#print the port you are about to scan
+		print'Scanning port' + tgtPort
+		#call the function that will scan the port
+		connScann(tgtHost, int(tgtPort))
+def main():
+	#create parser for script parameters
+	parser = optparse.OptionParser('usage%prog ' + '-H <target host> -p <target port>')
+	#add the options 
+	parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
+	parser.add_option('-p', dest='tgtPort', type='string', help='specify the target port[s] separated by comma and no space example 21,80,25')
+	#create the variable from the script parameters
+	(options, args) = parser.parse_args()
+	#create the tgtHost variable
+	tgtHost = options.tgtHost
+	#create the target ports variable
+	print options.tgtPort
+	tgtPorts = str(options.tgtPort).split(',')
+	print tgtPorts
+	#check if the variables have something in them
+	if (tgtHost == None) | (tgtPorts[0] == None):
+		#print error
+		print '[-] You must specify a target host and port[s].'
+		exit(0)
+	#call the port scan function
+	portScan(tgtHost, tgtPorts)
 
-	except Exception, e:
-		raise
-	else:
-		pass
-	finally:
-		pass
-
+if __name__ == '__main__':
+	main()
